d2910c0d38d1293a259fc3f8c470c8b2db573ab7
Michel Jansen <michel@lokku.com>
2012-07-04 16:23:46 +0100

    try to calculate parallax scroll speed dynamically

--- scripts/jquery.parallax-1.1.2.js
+++ scripts/jquery.parallax-1.1.2.js
@@ -16,6 +16,7 @@ http://www.gnu.org/licenses/gpl.html
 
   $window.resize(function () {
     windowHeight = $window.height();
+    console.log("Window Height:", windowHeight);
   });
 
   $.fn.parallax = function(adjuster, speedFactor, outerHeight) {
@@ -43,26 +44,31 @@ http://www.gnu.org/licenses/gpl.html
     function update(){
       var pos = $window.scrollTop();
       var rpos = Math.max(0, pos - top);
-      var targetpos = Math.min(980, -Math.round((adjuster - rpos) * speedFactor));
 
       $this.each(function(){
         var $element = $(this);
         var height = getHeight($element);
         var lcolbottom = $("#lcol").height() + $("#lcol").offset().top;
+
+        var rcolOverflow = Math.max(0, height - windowHeight);
+        var scrollDistance = Math.max(1,$("#lcol").height() - windowHeight);
+
+        var speedFactor = rcolOverflow / scrollDistance;
+
+        var targetpos = Math.round( rpos * speedFactor);
         var rcolbottom = getHeight($element)-targetpos + pos;
 
-        console.log(pos, targetpos, lcolbottom, rcolbottom);
+        console.log(pos, top, windowHeight, height, rcolOverflow, scrollDistance, speedFactor, rpos, targetpos);
 
         if(rcolbottom >= lcolbottom) {
           console.log("unstick");
           $this.css("position", "absolute");
           $this.css("top", $("#lcol").height() - $element.height() );
         }
-        else if(targetpos > 0) {
+        else if(rpos > 0) {
           $this.css("position", "fixed");
           $this.css("top", -targetpos+"px");
         }
-
         else {
           $this.css("position", "absolute");
           $this.css("top", "0");
